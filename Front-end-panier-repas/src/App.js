ca9d86e5aa3bc3aa6e89f537430e962cfab9f82e
Eyosias Woldemichael <eyosias16@gmail.com>
2024-04-24 15:15:05 +0200

    added front end and server side to deploy

---
+++ Front-end-panier-repas/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FileUploadForm from './components/FileUploadForm'; // Ensure this path is correct
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <h1 > Calcul d'Indémnités Panier repas heures sup: </h1>
+      <FileUploadForm />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ Front-end-panier-repas/src/components/FileUploadForm.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import './FileUploadForm.css'; // Make sure to import the CSS file
+function FileUploadForm() {
+  const [file, setFile] = useState(null);
+  const [fullName, setFullName] = useState('');
+  const [hourlyRate, setHourlyRate] = useState('');
+  const [PanRepas, setPanier] = useState('');
+  const [downloadLink, setDownloadLink] = useState(null); // State to hold the download link
+
+  const handleFileChange = (e) => setFile(e.target.files[0]);
+  const handleFullNameChange = (e) => setFullName(e.target.value);
+  const handleHourlyRateChange = (e) => setHourlyRate(e.target.value);
+  const handlePanierChange = (e) => setPanier(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('csvfile', file);
+    formData.append('fullname', fullName);
+    formData.append('hourlyrate', hourlyRate);
+    formData.append('PanRepas', PanRepas);
+
+    try {
+      const response = await fetch('http://127.0.0.1:5000/process-csv', {
+        method: 'POST',
+        body: formData,
+      });
+      if (!response.ok) throw new Error('Network response was not ok.');
+
+      // Get the filename from response headers if available
+      const filename = `${fullName}_Report.pdf`;
+
+      // Create a download link dynamically
+      const blob = await response.blob();
+      if (!blob) {
+        throw new Error('Blob object is null or undefined');
+      }
+      const downloadUrl = window.URL.createObjectURL(blob);
+      setDownloadLink({ url: downloadUrl, filename });
+      // const blob = await response.blob();
+      // const downloadUrl = window.URL.createObjectURL(blob);
+      // const link = document.createElement('a');
+      // link.href = downloadUrl;
+      // link.download = `${fullName}_Report.pdf`;
+      // document.body.appendChild(link);
+      // link.click();
+      // document.body.removeChild(link);
+    } catch (error) {
+      console.error('There has been a problem with your fetch operation:', error);
+    }
+  };
+
+  return (
+    <div className='form-container'>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input type="text" value={fullName} onChange={handleFullNameChange} placeholder="Nom et Prénom" required />
+        </div>
+        <div className="form-group val-use">
+          <input type="text" value={hourlyRate} onChange={handleHourlyRateChange} placeholder="Taux : e.g 11.07 €" />
+          <input type="text" value={PanRepas} onChange={handlePanierChange} placeholder="Ind repas : e.g 13.80€" />
+        </div>
+        <div className="form-group">
+          <input type="file" onChange={handleFileChange} accept=".csv" required />
+        </div>
+        <div className="form-group">
+          <button type="submit">Generate Report</button>
+        </div>
+      </form>
+      {downloadLink && (
+        <a href={downloadLink.url} download={downloadLink.filename} className="download-link">
+          Download PDF
+        </a>
+      )}
+    </div>
+  );
+}
+
+export default FileUploadForm;
