5b2cf7ab74c463ebca32a3aa58e3741f875cd15c
Eyosias Woldemichael <eyosias16@gmail.com>
2024-05-03 04:03:59 +0200

    started to add clause to not show the form with javascript

--- Front-end-panier-repas/src/App.js
+++ Front-end-panier-repas/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FileUploadForm from './components/FileUploadForm'; // Ensure this path is correct
 import InfoCard from './components/InfoCard'; // Import the InfoCard component
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Contact from './components/Contact';
+import PaymentComponent from './components/PaymentComponent';
 import "./App.css";
 function App() {
+  const [paymentComplete, setPaymentComplete] = useState(false);
+
+  // Function to handle payment completion
+  const handlePaymentCompletion = () => {
+    setPaymentComplete(true);
+  };
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <InfoCard />
-      <FileUploadForm />
+      {paymentComplete ? (
+        <FileUploadForm />
+      ): (
+        <React.Fragment>
+          <InfoCard />
+          <PaymentComponent onPaymentComplete={handlePaymentCompletion} />
+        </React.Fragment>
+      )}
+      <Contact />
     </div>
   );
 }
